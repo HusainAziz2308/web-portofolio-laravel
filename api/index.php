9042131afc954e903ff319872239cfd0a077943c
HusainAziz2308 <husainazizalrosyid@gmail.com>
2026-06-18 14:30:07 +0700

    fix tahap sekian

--- api/index.php
+++ api/index.php
@@ -1,2 +1,8 @@
 <?php
+
+declare(strict_types=1);
+
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+
 require __DIR__ . '/../public/index.php';
